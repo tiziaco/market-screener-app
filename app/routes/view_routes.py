7dfa7b0a7f57ce404eafb301e8f9d8d9b73909f9
tiziaco <tiziano.iaco@gmail.com>
2024-05-05 11:49:04 +0200

    fetch data from db

--- app/routes/view_routes.py
+++ app/routes/view_routes.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, render_template
+from app import wc
 
 views_blueprint = Blueprint('views', __name__)
 
 
 @views_blueprint.route('/')
 def index():
+    watchlist = wc.get_all_symbols()
+    table_data = [{'symbol': symbol_name, 'price': 0} for symbol_name in watchlist.values()]
 	# Example data for the table
-    table_data = [
-        {'symbol': 'BTCUSD', 'price': '$60,000'},
-        {'symbol': 'ETHUSD', 'price': '$2,500'},
-    ]
+    # table_data = [
+    #     {'symbol': 'BTCUSD', 'price': '$60,000'},
+    #     {'symbol': 'ETHUSD', 'price': '$2,500'},
+    # ]
     return render_template('index.html', table_data=table_data)
