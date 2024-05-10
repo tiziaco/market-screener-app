bed2587139f860c3c55109799bec7f86689ec03b
tiziaco <tiziano.iaco@gmail.com>
2024-05-10 22:19:47 +0200

    add itrader thread

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import os
 from threading import Thread
 from flask import request
 from dotenv import load_dotenv
-from itrader.trading_system.backtest_trading_system import TradingSystem
 from app import init_app, logger
 
 ## Load environment variables
 load_dotenv()
 
-app, socketio, ws, db = init_app()
+app, socketio, ws, db, itrader = init_app()
 
 with app.app_context():
 	db.create_all()
+
+#itrader = TradingSystem()
+stream_thread = Thread(target=ws.stream_data)
+stream_thread.start()
+
+itrader_thread = Thread(target=itrader.run)
+itrader_thread.start()
 
 @socketio.on('connect')
 def handle_connect():
@@ -23,10 +28,6 @@ def handle_connect():
 @socketio.on('disconnect')
 def handle_disconnect():
 	logger.info('SERVER: Client disconnected')
-
-itrader = TradingSystem()
-stream_thread = Thread(target=ws.stream_data)
-stream_thread.start()
 
 if __name__ == '__main__':
 	socketio.run(app, debug=True)
