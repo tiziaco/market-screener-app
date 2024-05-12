ec6bc0c74c1e8c1ad7408c81af34418d3f9b5570
tiziaco <tiziano.iaco@gmail.com>
2024-05-12 21:50:06 +0200

    update updatePriceInTable function

--- app/static/websocket_handler.js
+++ app/static/websocket_handler.js
@@ -2,37 +2,47 @@ const socket = io();
 
 // Function to change the color of the circle to green temporarily
 function changeCircleColorToGreen() {
-    $('.pingCircle').css('background-color', 'green');
+	$('.pingCircle').css('background-color', 'green');
 }
 
 // Function to revert the color of the circle back to dark green
 function revertCircleColorToDarkGreen() {
-    $('.pingCircle').css('background-color', 'darkgreen');
+	$('.pingCircle').css('background-color', 'darkgreen');
 }
 
-// Function to update the price in the table
-function updatePriceInTable(symbol, price) {
-    // Find the row in the table corresponding to the symbol
-    let $row = $(`#watchlist-table tbody tr:has(td:eq(1):contains('${symbol}'))`);
+function updatePriceInTable() {
+	// Retrieve the stored data from sessionStorage
+	let lastCloseData = JSON.parse(sessionStorage.getItem('lastClose'));
 
-    // Check if the row exists
-    if ($row.length > 0) {
-        // Update the price cell in the row
-        $row.find('td:eq(2)').text(price); // Update the third cell (index 2) with the new price
-    }
+	
+	// Iterate over each row in the table
+	$('#watchlist-table tbody tr').each(function() {
+		// Get the symbol from the first cell of the row
+		let symbol = $(this).find('td:eq(1)').text().trim();
+
+		// Check if the symbol exists in the lastCloseData
+		if (symbol in lastCloseData) {
+			// Update the price cell in the row with the corresponding price from lastCloseData
+			let price = lastCloseData[symbol];
+			$(this).find('td:eq(2)').text(price);
+		}
+	});
 }
 
 // Listen for live price updates
 socket.on('priceData', (data) => {
+	// Update the data stored in sessionStorage with the new data
+	sessionStorage.removeItem('lastClose');
+	sessionStorage.setItem('lastClose', JSON.stringify(data));
 
 	// Change the color of the circle to green temporarily
 	changeCircleColorToGreen();
-        
-	// Revert the color back to dark green after 300 milliseconds
-	setTimeout(revertCircleColorToDarkGreen, 100);
+	// Revert the color back to dark green after 200 milliseconds
+	setTimeout(revertCircleColorToDarkGreen, 200);
 
-    // Update the price in the watchlist table
-    updatePriceInTable(data.symbol, data.price);
+	// Update the price in the watchlist table
+	updatePriceInTable();
 
-	// console.log(`Symbol: ${data.symbol}, Price: ${data.price}`)
+	// Update the best-worst performing screener
+	update_performance_table();
 });
